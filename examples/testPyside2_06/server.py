a0dbc40091a31dbd1e030ee37243968bf9912525
hooloong <hooloong@vip.qq.com>
2018-04-22 23:02:20 +0800

    test win32pipe.

---
+++ examples/testPyside2_06/server.py
@@ -0,0 +1,31 @@
+import win32file
+import win32pipe
+
+PIPE_NAME = r'\\.\pipe\test_pipe'
+PIPE_BUFFER_SIZE = 65535
+
+while True:
+    named_pipe = win32pipe.CreateNamedPipe(PIPE_NAME,
+                                           win32pipe.PIPE_ACCESS_DUPLEX,
+                                           win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_WAIT | win32pipe.PIPE_READMODE_MESSAGE,
+                                           win32pipe.PIPE_UNLIMITED_INSTANCES,
+                                           PIPE_BUFFER_SIZE,
+                                           PIPE_BUFFER_SIZE, 500, None)
+    try:
+        while True:
+            try:
+                win32pipe.ConnectNamedPipe(named_pipe, None)
+                data = win32file.ReadFile(named_pipe, PIPE_BUFFER_SIZE, None)
+
+                if data is None or len(data) < 1:
+                    continue
+
+                print( 'receive msg:', data)
+            except BaseException as e:
+                print ( "exception:", e)
+                break
+    finally:
+        try:
+            win32pipe.DisconnectNamedPipe(named_pipe)
+        except:
+            pass
